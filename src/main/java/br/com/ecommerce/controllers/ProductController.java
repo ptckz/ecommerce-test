92e4baf4b0ce27a292fd77e5245df42d8716088c
Patrick <patrickantonioliw@gmail.com>
2018-05-15 16:24:36 -0300

    gitignore and response entity

--- src/main/java/br/com/ecommerce/controllers/ProductController.java
+++ src/main/java/br/com/ecommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package br.com.ecommerce.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +25,9 @@ public class ProductController {
 	ProductService productService;
 	
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Product create(@RequestBody Product input){
-        return productService.createNewProduct(input);
+    public ResponseEntity<?> create(@RequestBody Product input){
+    	Product product = productService.createNewProduct(input);
+        return new ResponseEntity<Product>(product, HttpStatus.CREATED);
     }
     
     @ResponseBody
@@ -33,20 +36,27 @@ public class ProductController {
     	return productService.findAll();
     }
     
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getUser(@PathVariable("id") long id) {
+    	Product product = productService.findById(id);
+        return new ResponseEntity<Product>(product, HttpStatus.OK);
+    }
+    
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteProduct(@PathVariable("id") long id){
         productService.deleteProduct(id);
     }
     
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public Product updateProduct(@PathVariable("id") long id, @RequestBody Product product){
+    public ResponseEntity<?> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
     	Product currentProduct = productService.findById(id);
     	
     	currentProduct.setName(product.getName());
     	currentProduct.setValue(product.getValue());
+    	Product productUpdate = productService.updateProduct(currentProduct);
     	
-        return productService.updateProduct(currentProduct);
+        return new ResponseEntity<Product>(productUpdate, HttpStatus.OK);
+        
     }
-    
 	
 }
